95e3a8b64d1b93ba51d33a707cad39f9f4b73df5
Max Duval <max.duval@theguardian.com>
2023-05-18 11:42:08 +0100

    feat: add data as JSON

--- src/recipes.js
+++ src/recipes.js
@@ -1,5 +1,5 @@
 import { recipes_schema } from './parser.js';
-import raw_data from './data/recipes_table_export.json';
+import raw_data from './recipes_table_export.json';
 
 export const recipes = recipes_schema
 	.parse(raw_data)
