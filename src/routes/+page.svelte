5e9fda10a6640677b2fa0737823e89f6379bf756
Max Duval <max.duval@theguardian.com>
2023-05-18 08:07:57 +0100

    feat: focus on ingredients

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,7 @@
 	export let data;
 
 	let search = '';
-
-	$: selection = /** @type {Set<string>} */ (new Set());
+	let selection = /** @type {Set<string>} */ (new Set());
 
 	$: list = data.recipes.filter(({ ingredients_lists }) =>
 		ingredients_lists.some(({ ingredients: listed }) =>
@@ -20,78 +19,116 @@
 	$: ingredients_list = [...ingredients]
 		.filter(([name]) => !selection.has(name) && name.includes(search))
 		.sort((a, b) => b[1] - a[1]);
+
+	/** @param {string} ingredient */
+	const add_ingredient = (ingredient) => {
+		selection.add(ingredient);
+		selection = selection;
+		search = '';
+	};
+
+	/** @type {import('./$types').Snapshot<{search: string, selection: Set<string>}>} */
+	export const snapshot = {
+		capture: () => ({ search, selection }),
+		restore: ({ search: search_snapshot, selection: selection_snapshot }) => {
+			selection = selection_snapshot;
+			search = search_snapshot;
+		},
+	};
 </script>
 
 <Header />
 
-<p>
-	Found {ingredients.size} individual ingredients
-</p>
-
-<input
-	type="search"
-	bind:value={search}
-	on:keydown={(event) => {
-		if (event.key === 'Enter') {
-			event.preventDefault();
-			const [ingredient] = ingredients_list[0] ?? [];
-			if (ingredient) {
-				selection.add(ingredient);
-				selection = selection;
-				search = '';
+<label>
+	<h3>What ingredients do you have?</h3>
+	<input
+		type="search"
+		placeholder="type to search…"
+		bind:value={search}
+		on:keydown={(event) => {
+			if (event.key === 'Enter') {
+				event.preventDefault();
+				const [ingredient] = ingredients_list[0] ?? [];
+				if (ingredient) add_ingredient(ingredient);
 			}
-		}
-	}}
-/>
+		}}
+	/>
+</label>
 
-{#if selection.size > 0}
+<ul>
 	{#each [...selection] as ingredient}
 		<button
+			class="ingredient selected"
 			on:click={() => {
 				selection.delete(ingredient);
 				selection = selection;
+				search = '';
 			}}
 		>
 			{ingredient} &cross;
 		</button>
 	{/each}
-{/if}
-
-{#if list.length > 3}
-	<ul>
-		{#each ingredients_list as [name, count]}
+	{#if list.length > 5}
+		{#each ingredients_list.slice(0, 12) as [name], index}
 			<li>
 				<button
+					class="ingredient"
+					class:first={index === 0}
 					on:click={() => {
-						selection.add(name);
-						selection = selection;
-					}}
-					class="ingredient">{name} ({count})</button
+						add_ingredient(name);
+					}}>{name} &cir;</button
 				>
 			</li>
 		{/each}
-	</ul>
-{/if}
+		{#if ingredients_list.length > 12}
+			<li>and {ingredients_list.length - 12} more…</li>
+		{/if}
+	{/if}
+</ul>
 
 {#if selection.size > 0}
 	<Recipes {list} />
 {/if}
 
 <style>
+	label {
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		padding: 2rem;
+	}
+	input[type='search'] {
+		font-size: 2rem;
+		text-align: center;
+	}
 	ul {
 		list-style-type: none;
 		display: flex;
 		flex-wrap: wrap;
-		row-gap: 1rem;
+		gap: 1rem;
+		justify-content: center;
+		align-items: center;
+		padding: 1rem;
 	}
 
 	.ingredient {
+		font-size: 1.5rem;
+		background-color: rgb(254, 238, 247);
 		color: #bb3b80;
+		border: currentColor solid 2px;
+		border-radius: 2rem;
+		padding: 0.25em 0.5em;
+		box-sizing: border-box;
+		height: 3.6rem;
 	}
 
-	li::after {
-		content: '/';
-		margin: 0 0.5rem;
-		color: #707070;
+	.ingredient.first {
+		border-width: 4px;
+	}
+
+	.ingredient.selected {
+		background-color: #bb3b80;
+		border-color: #7d0068;
+		color: rgb(254, 238, 247);
 	}
 </style>
