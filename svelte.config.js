9b657702c91ebede84b2a9cc4cb183e7475c4669
Max Duval <max.duval@theguardian.com>
2023-05-19 09:09:19 +0100

    feat: let’s try edge functions?

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import adapter from '@sveltejs/adapter-netlify';
 const config = {
 	kit: {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({ edge: true }),
 	},
 };
 
