fbab2e696d2b24e93506b4e8893728e9e91afca0
Max Duval <max.duval@theguardian.com>
2023-05-19 09:55:29 +0100

    feat: order recipes by ingredients

--- src/routes/all/+page.server.js
+++ src/routes/all/+page.server.js
@@ -1,8 +1,21 @@
+import { normalise } from '../../lib/normalise.js';
 import { recipes } from '../../lib/recipes.js';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
 	return {
-		recipes,
+		recipes: recipes
+			.map(({ path, recipeId, recipes_title, ingredients_lists }) => ({
+				path,
+				recipeId,
+				recipes_title,
+				ingredients: ingredients_lists.reduce((set, { ingredients }) => {
+					for (const { item } of ingredients) {
+						set.add(normalise(item));
+					}
+					return set;
+				}, /** @type {Set<string>} */ (new Set())).size,
+			}))
+			.sort(({ ingredients: a }, { ingredients: b }) => a - b),
 	};
 }
--- src/routes/all/+page.svelte
+++ src/routes/all/+page.svelte
@@ -3,7 +3,14 @@
 </script>
 
 <ul>
-	{#each data.recipes as recipe}
-		<li><a href={`${recipe.path}/${recipe.recipeId}`}>{recipe.recipes_title}</a></li>
+	{#each data.recipes as recipe, index}
+		{#if data.recipes[index - 1]?.ingredients !== recipe.ingredients}
+			<h3>
+				Recipes with {recipe.ingredients} ingredients
+			</h3>
+		{/if}
+		<li>
+			<a href={`${recipe.path}/${recipe.recipeId}`}>{recipe.recipes_title}</a>
+		</li>
 	{/each}
 </ul>
